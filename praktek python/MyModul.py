654a8a65594da54da4f0e54209a197ae90eef869
Najmi S.A <117152004+starlight22106@users.noreply.github.com>
2022-11-30 17:29:38 +0700

    Add files via upload

---
+++ praktek python/MyModul.py
@@ -0,0 +1,16 @@
+import math
+def tambah(bil1,bil2):
+    hasil = bil1 + bil2
+    print("hasil tambah dari",bil1,"+",bil2,"=",hasil) 
+def kurang(bil1,bil2):
+    hasil = bil1 - bil2
+    print("hasil kurang dari",bil1,"-",bil2,"=",hasil) 
+def kali(bil1,bil2):
+    hasil = bil1 * bil2
+    print("hasil kali dari",bil1,"x",bil2,"=",hasil)
+def bagi(bil1,bil2):
+    hasil = bil1 / bil2
+    print("hasil bagi dari",bil1,":",bil2,"=",hasil)
+def pangkat(bil1,bil2):
+    hasil = math.pow(bil1,bil2)
+    print("hasil panggil dari",bil1,"^",bil2,"=",hasil)
---
+++ praktek python/useModul.py
@@ -0,0 +1,11 @@
+print("----gunakan modul-----")
+import MyModul
+MyModul.tambah(5,2)
+MyModul.kurang(9,7)
+MyModul.kali(10,2)
+MyModul.bagi(50,5)
+MyModul.pangkat(2,2)
+
+print("mengubah penamaan modul")
+import MyModul as perhitungan
+perhitungan.tambah(2,4)
